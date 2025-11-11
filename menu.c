42732b0a6f9de48dd5e3620a28860b68244d99f6
CarvDev <arthurcarvlima@outlook.com>
2025-11-11 20:42:34 -0300

    Melhorada a função obter_opcao()

--- menu.c
+++ menu.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "menu.h"
+#include "auxiliar.h"
 
 void mostra_menu() {
     printf("Digite a opcao desejada:\n"
@@ -12,19 +13,33 @@ void mostra_menu() {
 
 int obter_opcao(int limite) {
     int opc;
-    while (1) {
-        opc = 0; // (re)definindo opção como 0, por segurança
-        
-        printf("> "); // caractere '>' para melhorar a exibição no terminal
-        scanf("%d", &opc);
+    int scanf_retorno; // Variável para checar o retorno do scanf
 
-        // verificando se a opção digitada está dentro do escopo do menu 
-        if (opc >= 1 && opc <= limite) {
-            break; // se a opção estiver dentro escopo, quebra o loop
-        } else {
-            // caso estiver fora, imprime mensagem de erro e continua no loop
+    while (1) {
+        printf("> "); // caractere '>' para uma melhor exibição do programa no terminal
+        
+        // Armazena o valor de retorno do scanf
+        scanf_retorno = scanf("%d", &opc);
+
+        // Caso 1: O input NÃO FOI UM NÚMERO (ex: 'a')
+        // scanf_retorno será 0
+        if (scanf_retorno == 0) {
+            printf("[Erro: Digite apenas números]\nTente novamente...\n\n");
+            limpar_buffer(); // Limpa o input inválido (ex: 'a\n')
+            continue; // Pula para a próxima iteração do loop
+        }
+
+        // Caso 2: FOI UM NÚMERO (scanf_retorno == 1), mas está FORA DO LIMITE
+        if (opc < 1 || opc > limite) {
             printf("[Opção inválida]\nTente novamente...\n\n");
         }
+        
+        // Caso 3: SUCESSO! É um número e está dentro do limite
+        if (opc >= 1 && opc <= limite) {
+            break; // Quebra o loop
+        }
     }
+    
+    limpar_buffer(); // para limpar o '\n' no buffer, para não quebrar o fgets().
     return opc;
 }
