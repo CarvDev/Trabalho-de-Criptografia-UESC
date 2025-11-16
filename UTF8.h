d912209d3466bb56ed2a285f98ab302bb8f72033
CarvDev <arthurcarvlima@outlook.com>
2025-11-15 21:12:23 -0300

    Arquivo para mapear os caracteres acentuados

---
+++ UTF8.h
@@ -0,0 +1,56 @@
+#ifndef UTF8_H
+#define UTF8_H
+
+// Constantes UTF-8
+
+// Minúsculos
+// Til
+#define a_til 0xA3 // ã
+#define o_til 0xB5 // õ
+// Agudo
+#define a_agudo 0xA1 // á
+#define e_agudo 0xA9 // é
+#define i_agudo 0xAD // í
+#define o_agudo 0xB3 // ó
+#define u_agudo 0xBA // ú
+// Circunflexo
+#define a_circ 0xA2 // â
+#define e_circ 0xAA // ê
+#define i_circ 0xAE // î
+#define o_circ 0xB4 // ô
+#define u_circ 0xBB // û
+// Grave
+#define a_grave 0xA0 // à
+#define e_grave 0xA8 // è
+#define i_grave 0xAC // ì
+#define o_grave 0xB2 // ò
+#define u_grave 0xB9 // ù
+// Cedilha
+#define c_cedilha 0xA7 // ç
+
+// Maiúsculos
+// Til
+#define A_TIL 0x83 // Ã
+#define O_TIL 0x95 // Õ
+// Agudo
+#define A_AGUDO 0x81 // Á
+#define E_AGUDO 0x89 // É
+#define I_AGUDO 0x8D // Í
+#define O_AGUDO 0x93 // Ó
+#define U_AGUDO 0x9A // Ú
+// Circunflexo
+#define A_CIRC 0x82 // Â
+#define E_CIRC 0x8A // Ê
+#define I_CIRC 0x8E // Î
+#define O_CIRC 0x94 // Ô
+#define U_CIRC 0x9B // Û
+// Grave
+#define A_GRAVE 0x80 // À
+#define E_GRAVE 0x88 // È
+#define I_GRAVE 0x8C // Ì
+#define O_GRAVE 0x92 // Ò
+#define U_GRAVE 0x99 // Ù
+// Cedilha
+#define C_CEDILHA 0x87 // Ç
+
+#endif
